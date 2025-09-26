d92169922705c8e7668c0a0feaf4417c760c2b05
hosiuhangdesmond <hosiuhangdesmond@icloud.com>
2025-09-26 14:25:15 +0800

    Step 6: navbar links update

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    print(request.path)
     return render(request, 'pages/index.html')
 
 def about(request):
+    print(request.path)
     return render(request, 'pages/about.html')
