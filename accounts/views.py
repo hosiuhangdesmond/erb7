7e13c7c19aa8791c090a6920acdddfe40b428d3a
hosiuhangdesmond <hosiuhangdesmond@icloud.com>
2025-10-13 12:14:09 +0800

    Step 16: message

---
+++ accounts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        print("submit")
+        messages.success(request, "Account created successfully")
+        #messages.error(request, "Account created unsuccessfully")
+        # debug messages content
+        #storage = messages.get_messages(request)
+        #for message in storage:
+            #print(message.level_tag, message.message)
+        print(messages)
+        return redirect('accounts:register')
+    else:
+        return render(request, 'accounts/register.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return redirect("pages:index")
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+from django.contrib.messages import constants as messages
 load_dotenv()
 
 
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'doctors.apps.DoctorsConfig',
     'taggit',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +147,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 TAGGIT_CASE_INSENSITIVE =True
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
